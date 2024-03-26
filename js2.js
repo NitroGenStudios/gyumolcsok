edab272cbf85d2ddfaaf968ae4c88c07182ec7c7
Your Name <you@example.com>
2024-03-26 14:20:26 +0100

    aaaaaaaaa2

--- js2.js
+++ js2.js
@@ -4,6 +4,11 @@ function selectaaa(selector)
 
 	let gyumolcs = fruits.find(obj => obj.id == selector.value)
 
+	if (gyumolcs == null)
+	{
+		selector.classList.toggle("hidden") // aaaaaa
+	}
+
 	document.getElementById("title").innerText = gyumolcs.name;
 	document.getElementById("description").innerText = gyumolcs.descr;
 	document.getElementById("gyumolcskep").src = `img/${gyumolcs.image}`;
